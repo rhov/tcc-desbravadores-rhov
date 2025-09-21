d14effb2278d5b4822c2e1a91d57f75d838a4175
Rodrigo Henrique <rodrigo.henrique@iatec.com>
2025-09-21 19:03:12 -0300

    Add fixture files and update tests for buscaUnidade functionality

--- test/graphql/external/busca/unidade.test.js
+++ test/graphql/external/busca/unidade.test.js
@@ -7,112 +7,125 @@ let token;
 require('dotenv').config();
 
 describe.only('Clube de Desbravadores - Testes External', () => {
-    beforeEach(async () => {
-        token = await loginGraphql();
-        buscaUnidade = require('../../../fixture/requisicoes/busca/buscaUnidade.fixture.json');
-    });
+
 
     describe('Busca Unidade', () => {
+        let buscaUnidadeDadosValidos = require('../../../fixture/requisicoes/busca/buscaUnidadeDadosValidos.fixture.json');
+        let buscaUnidadeComErros = require('../../../fixture/requisicoes/busca/buscaUnidadeComErros.fixture.json');
+
+        beforeEach(async () => {
+            token = await loginGraphql();
+        });
 
         it('Buscar unidade sem token', async () => {
             const respostaEsperada = require('../../../fixture/respostas/token/tokenAusente.fixture.json');
             const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
-                .send(buscaUnidade);
+                .send(buscaUnidadeDadosValidos[0].buscaUnidade);
             expect(resposta.body.errors[0].message).to.equals(respostaEsperada.message);
+
         });
 
-        it('Buscar unidade (Unidade e Clube) com dados válidos ', async () => {
-            const respostaEsperada = require('../../../fixture/respostas/busca/unidade/dadosValidosUnidadeClube.fixture.json');
-            const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
-                .set('Authorization', `Bearer ${token}`)
-                .send(buscaUnidade);
-            expect(resposta.body).to.eql(respostaEsperada);
-        });
-
-        it('Buscar unidade (Unidade) com dados válidos ', async () => {
-            const respostaEsperada = require('../../../fixture/respostas/busca/unidade/dadosValidosUnidade.fixture.json');
-            delete buscaUnidade.variables.clubeNome;
-            const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
-                .set('Authorization', `Bearer ${token}`)
-                .send(buscaUnidade);
-            expect(resposta.body).to.eql(respostaEsperada);
-        });
-
-        it('Buscar todas as unidades de um clube com dados válidos ', async () => {
-            const respostaEsperada = require('../../../fixture/respostas/busca/unidade/dadosValidosClube.fixture.json');
-            delete buscaUnidade.variables.unidade;
-            const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
-                .set('Authorization', `Bearer ${token}`)
-                .send(buscaUnidade);
-            expect(resposta.body).to.eql(respostaEsperada);
-        });
+        buscaUnidadeDadosValidos.forEach(teste => {
+            it.only(`${teste.nomeDoTeste}`, async () => {
+                const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
+                    .set('Authorization', `Bearer ${token}`)
+                    .send(teste.buscaUnidade);
+                expect(resposta.body).to.eql(teste.resultadoEsperado);
+            });
+        })
 
         it('Buscar com dados obrigatórios vazios (clube e unidade)', async () => {
             const respostaEsperada = require('../../../fixture/respostas/busca/unidade/dadosNaoInformados.fixture.json');
-            buscaUnidade.variables.unidade = '';
-            buscaUnidade.variables.clubeNome = '';
+            buscaUnidadeComErros.variables.unidade = '';
+            buscaUnidadeComErros.variables.clubeNome = '';
             const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
                 .set('Authorization', `Bearer ${token}`)
-                .send(buscaUnidade);
+                .send(buscaUnidadeComErros);
             expect(resposta.body.errors[0].message).to.equals(respostaEsperada.message);
         });
 
         it('Buscar com dados obrigatórios vazios (clube)', async () => {
             const respostaEsperada = require('../../../fixture/respostas/busca/unidade/dadosNaoInformados.fixture.json');
-            delete buscaUnidade.variables.unidade;
-            buscaUnidade.variables.clubeNome = '';
+            delete buscaUnidadeComErros.variables.unidade;
+            buscaUnidadeComErros.variables.clubeNome = '';
             const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
                 .set('Authorization', `Bearer ${token}`)
-                .send(buscaUnidade);
+                .send(buscaUnidadeComErros);
             expect(resposta.body.errors[0].message).to.equals(respostaEsperada.message);
         });
 
         it('Buscar com dados obrigatórios vazios (unidade)', async () => {
             const respostaEsperada = require('../../../fixture/respostas/busca/unidade/dadosNaoInformados.fixture.json');
-            buscaUnidade.variables.unidade = '';
-            delete buscaUnidade.variables.clubeNome;
+            buscaUnidadeComErros.variables.unidade = '';
+            delete buscaUnidadeComErros.variables.clubeNome;
             const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
                 .set('Authorization', `Bearer ${token}`)
-                .send(buscaUnidade);
+                .send(buscaUnidadeComErros);
             expect(resposta.body.errors[0].message).to.equals(respostaEsperada.message);
         });
 
         it('Buscar com dados obrigatórios null (clube e unidade)', async () => {
             const respostaEsperada = require('../../../fixture/respostas/busca/unidade/dadosNaoInformados.fixture.json');
-            buscaUnidade.variables.unidade = null;
-            buscaUnidade.variables.clubeNome = null;
+            buscaUnidadeComErros.variables.unidade = null;
+            buscaUnidadeComErros.variables.clubeNome = null;
             const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
                 .set('Authorization', `Bearer ${token}`)
-                .send(buscaUnidade);
+                .send(buscaUnidadeComErros);
             expect(resposta.body.errors[0].message).to.equals(respostaEsperada.message);
         });
 
         it('Buscar com dados obrigatórios null (clube)', async () => {
             const respostaEsperada = require('../../../fixture/respostas/busca/unidade/dadosNaoInformados.fixture.json');
-            delete buscaUnidade.variables.unidade;
-            buscaUnidade.variables.clubeNome = null;
+            delete buscaUnidadeComErros.variables.unidade;
+            buscaUnidadeComErros.variables.clubeNome = null;
             const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
                 .set('Authorization', `Bearer ${token}`)
-                .send(buscaUnidade);
+                .send(buscaUnidadeComErros);
             expect(resposta.body.errors[0].message).to.equals(respostaEsperada.message);
         });
 
         it('Buscar com dados obrigatórios null (unidade)', async () => {
             const respostaEsperada = require('../../../fixture/respostas/busca/unidade/dadosNaoInformados.fixture.json');
-            buscaUnidade.variables.unidade = null;
-            delete buscaUnidade.variables.clubeNome;
+            buscaUnidadeComErros.variables.unidade = null;
+            delete buscaUnidadeComErros.variables.clubeNome;
             const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
                 .set('Authorization', `Bearer ${token}`)
-                .send(buscaUnidade);
+                .send(buscaUnidadeComErros);
             expect(resposta.body.errors[0].message).to.equals(respostaEsperada.message);
         });
 
-        it('Buscar clube  inexistente', async () => {
-            const respostaEsperada = require('../../../fixture/respostas/busca/clube/nomeNaoEncontrado.json');
-            buscaUnidade.variables.nome = 'nomeNaoEncontrado';
+        it('Unidade não encontrada por unidade', async () => {
+            const respostaEsperada = require('../../../fixture/respostas/busca/unidade/unidadeNaoEncontrada.fixture.json');
+            buscaUnidadeComErros.variables.unidade = 'nomeNaoEncontrado';
             const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
                 .set('Authorization', `Bearer ${token}`)
-                .send(buscaUnidade);
+                .send(buscaUnidadeComErros);
+            expect(resposta.body.errors[0].message).to.equals(respostaEsperada.message);
+        });
+        it('Unidade não encontrada por clube', async () => {
+            const respostaEsperada = require('../../../fixture/respostas/busca/unidade/unidadeNaoEncontrada.fixture.json');
+            buscaUnidadeComErros.variables.clubeNome = 'nomeNaoEncontrado';
+            const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
+                .set('Authorization', `Bearer ${token}`)
+                .send(buscaUnidadeComErros);
+            expect(resposta.body.errors[0].message).to.equals(respostaEsperada.message);
+        });
+        it('Unidade não encontrada por clube válido e unidade inexsitente', async () => {
+            const respostaEsperada = require('../../../fixture/respostas/busca/unidade/unidadeNaoEncontrada.fixture.json');
+            buscaUnidadeComErros.variables.clubeNome = 'sacerdotes';
+            buscaUnidadeComErros.variables.unidade = 'desconehcida';
+            const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
+                .set('Authorization', `Bearer ${token}`)
+                .send(buscaUnidadeComErros);
+            expect(resposta.body.errors[0].message).to.equals(respostaEsperada.message);
+        });
+        it('Unidade não encontrada por unidade válida e clube inexsitente', async () => {
+            const respostaEsperada = require('../../../fixture/respostas/busca/unidade/unidadeNaoEncontrada.fixture.json');
+            buscaUnidadeComErros.variables.clubeNome = 'desconhecida';
+            buscaUnidadeComErros.variables.unidade = 'melquisedeque';
+            const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
+                .set('Authorization', `Bearer ${token}`)
+                .send(buscaUnidadeComErros);
             expect(resposta.body.errors[0].message).to.equals(respostaEsperada.message);
         });
 
