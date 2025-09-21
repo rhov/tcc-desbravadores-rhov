1bc1785008c8419f2da4d2676e19d3c25230f85c
Rodrigo Henrique <rodrigo.henrique@iatec.com>
2025-09-21 00:55:22 -0300

    Refactor test setup for clubeExternal; implement buscaDesbravador test and loginGraphql function for authentication

--- test/graphql/external/clubeExternal.test.js
+++ test/graphql/external/clubeExternal.test.js
@@ -1,5 +1,36 @@
 const request = require('supertest');
-const {expect} = require('chai');
-const app = require('../../../app');
+const { expect } = require('chai');
+const { query } = require('express');
+const {loginGraphql} = require('../../factory/requisicoes/login/login');
+let token;
 
-console.log(app);
+require('dotenv').config();
+
+describe('Desbravador', () => {
+    beforeEach(async ()  => {
+        token = await loginGraphql();        
+    });
+
+    it('buscaDesbravador', async () => {
+        const respostaEsperada = require('../../fixture/respostas/buscaDesbravador.json');
+        const resposta = await request(process.env.BASE_URL_GRAPHQL)
+            .post('')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                query: `query BuscarDesbravador($documento: String!) {
+                    buscarDesbravador(documento: $documento) {
+                        nome
+                        idade
+                        documento
+                        clubeNome
+                        unidadeNome
+                    }
+                }`,
+                variables: {
+                    documento: "005"
+                }
+            });
+        expect(resposta.body).to.eql(respostaEsperada);
+        //console.log(resposta.body);
+    })
+});
