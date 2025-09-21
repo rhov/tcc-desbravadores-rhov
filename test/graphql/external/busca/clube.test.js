3429b8a544a87e192a23385830d4dcff817091b5
Rodrigo Henrique <rodrigo.henrique@iatec.com>
2025-09-21 15:35:21 -0300

    Remove unused import of buscarClube from clubeController in clube.test.js

--- test/graphql/external/busca/clube.test.js
+++ test/graphql/external/busca/clube.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const { expect } = require('chai');
 const { loginGraphql } = require('../../../factory/requisicoes/login/login');
-const { buscarClube } = require('../../../../controller/clubeController');
+
 let token;
 
 require('dotenv').config();
