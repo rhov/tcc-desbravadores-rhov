8064ed67a92560058924fbd2062912ab5d06fa37
Rodrigo Henrique <rodrigo.henrique@iatec.com>
2025-09-21 22:51:09 -0300

    Add tests for creating a Clube with various validation scenarios

--- test/graphql/external/mutation/criarClube.test.js
+++ test/graphql/external/mutation/criarClube.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const { expect } = require('chai');
 const { loginGraphql } = require('../../../factory/requisicoes/login/login');
+let criarClube = require('../../../fixture/requisicoes/mutation/criarClube/clubeDadosErro.fixture.json');
 let token;
 
 require('dotenv').config();
@@ -40,86 +41,14 @@ describe('Clube de Desbravadores - Testes External', () => {
 
         });
 
-        it('Registrar clube com dados faltando (unidades)', async () => {
-            const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
-                .set('Authorization', `Bearer ${token}`)
-                .send({
-                    query: `mutation CriarClube($nome: String!, $unidades: [String!]) {
-                                criarClube(nome: $nome, unidades: $unidades) {
-                                    id
-                                    nome
-                                    unidades
-                                }
-                            }`,
-                    variables: {
-                        nome: "Paineiras",
-                        unidades: null
-                    }
-                });
-            expect(resposta.body.errors[0].message).to.eql("Obrigatório informar os campos nome e unidades (ao menos uma)!");
-
-        });
-
-        it('Registrar clube com dados faltando (nome vazio)', async () => {
-            const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
-                .set('Authorization', `Bearer ${token}`)
-                .send({
-                    query: `mutation CriarClube($nome: String!, $unidades: [String!]) {
-                                criarClube(nome: $nome, unidades: $unidades) {
-                                    id
-                                    nome
-                                    unidades
-                                }
-                            }`,
-                    variables: {
-                        nome: "",
-                        unidades: "Corcovado"
-                    }
-                });
-            expect(resposta.body.errors[0].message).to.eql("Obrigatório informar os campos nome e unidades (ao menos uma)!");
-
-        });
-
-        it('Registrar clube com duas unidades iguais', async () => {
-            const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
-                .set('Authorization', `Bearer ${token}`)
-                .send({
-                    query: `mutation CriarClube($nome: String!, $unidades: [String!]) {
-                                criarClube(nome: $nome, unidades: $unidades) {
-                                    id
-                                    nome
-                                    unidades
-                                }
-                            }`,
-                    variables: {
-                        nome: "Paineiras",
-                        unidades: ["Corcovado", "Corcovado"]
-                    }
-                });
-            expect(resposta.body.errors[0].message).to.eql("Não pode haver duas unidades com o mesmo nome no mesmo clube");
-
-        });
-
-
-        /*
-                buscaUnidadeDadosValidos.forEach(teste => {
-                    it(`${teste.nomeDoTeste}`, async () => {
-                        const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
-                            .set('Authorization', `Bearer ${token}`)
-                            .send(teste.buscaUnidade);
-                        expect(resposta.body).to.eql(teste.resultadoEsperado);
-                    });
-                })
-        
-                buscaUnidadeComErros.forEach(teste => {
-                    it(`${teste.nomeDoTeste}`, async () => {
-                        const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
-                            .set('Authorization', `Bearer ${token}`)
-                            .send(teste.buscaUnidade);
-                        expect(resposta.body.errors[0].message).to.eql(teste.resultadoEsperado);
-                    });
-                })
-        */
+        criarClube.forEach(teste => {
+            it(`${teste.nomeDoTeste}`, async () => {
+                const resposta = await request(process.env.BASE_URL_GRAPHQL).post('')
+                    .set('Authorization', `Bearer ${token}`)
+                    .send(teste.criarClube);
+                expect(resposta.body.errors[0].message).to.eql(teste.resultadoEsperado);
+            });
+        })
 
     });
 });
