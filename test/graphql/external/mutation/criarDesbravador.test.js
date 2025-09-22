42c4adc150131902423b2114054ca1a6e7964d2a
Rodrigo Henrique <rodrigo.henrique@iatec.com>
2025-09-21 22:43:46 -0300

    Remove unused faker dependency from criarDesbravador test file

--- test/graphql/external/mutation/criarDesbravador.test.js
+++ test/graphql/external/mutation/criarDesbravador.test.js
@@ -1,6 +1,5 @@
 const request = require('supertest');
 const { expect } = require('chai');
-const { faker } = require('@faker-js/faker');
 const { loginGraphql } = require('../../../factory/requisicoes/login/login');
 let criarDbv = require('../../../fixture/requisicoes/mutation/criarDesbravador/dbvDadosValidos.fixture.json');
 let criarDbvValidacoes = require('../../../fixture/requisicoes/mutation/criarDesbravador/dbvDadosErro.fixture.json');
